dcc5da78bda5c81656a62b12dfef6718c68fec2f
rjustinfreeman <rjustinfreeman@gmail.com>
2025-04-29 09:49:56 -0300

    removed noise background

--- src/components/react/noise-background.tsx
+++ src/components/react/noise-background.tsx
@@ -1,5 +1,5 @@
 export function NoiseBackground() {
   return (
-    <div className="fixed inset-0 z-[-1] h-full w-full bg-[url('/noise.png')] bg-[length:128px_128px] bg-repeat opacity-[0.06]"></div>
+    <div className="fixed inset-0 z-[-1] h-full w-full bg-white dark:bg-[#1c1c1c]"></div>
   )
 }
